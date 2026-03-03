603aa32c242fc86ff28b527f3f96023b18e8d0fc
cyn <letcindyknow@gmail.com>
2026-03-03 15:40:53 -0500

    oops vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // Change '/green-grapes/' to match your GitHub repo name
-  base: "/green-grapes/",
+  base: "/cyn.github.io/",
 });
